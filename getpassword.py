446423995a40daa6cf3e991b59fa32998354636c
sultumov <sultumovb@gmail.com>
2024-03-28 20:21:24 +0800

    change getpassword.py

--- getpassword.py
+++ getpassword.py
@@ -7,7 +7,7 @@ def get_random_uppercase():
 def get_random_digit():
     return random.randint(0, 9)
 def get_random_special_char():
-    special_chars = '!@#$%^&*()'
+    special_chars = '!"#$%\'&*()'
     return random.choice(special_chars)
 def get_random_lowercase():
     alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
@@ -17,13 +17,15 @@ def get_random_lowercase():
 def newpass(identifier):
     n = len(identifier)
     if n > 0:
-        first_char = get_random_uppercase()
-        second_char = get_random_uppercase()
-        third_char = n*n % 10
-        fourth_char = get_random_digit()
+        first_char = get_random_digit()
+        second_char = get_random_digit()
+        third_char = get_random_digit()
+        fourth_char = get_random_special_char()
         fifth_char = get_random_special_char()
-        sixth_char = get_random_lowercase()
+        sixth_char = get_random_special_char()
+        seven_char = get_random_lowercase()
+        eight_char = get_random_uppercase()
 
-        password = first_char + second_char + str(third_char) + str(fourth_char) + fifth_char + sixth_char
+        password = first_char + second_char + str(third_char) + str(fourth_char) + fifth_char + sixth_char+seven_char+eight_char
 
         return password
