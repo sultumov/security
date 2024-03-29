a18b0afde447ac51d5898f9c6176b5a4349b88b3
sultumov <sultumovb@gmail.com>
2024-03-29 20:58:20 +0800

    update sql file

--- sql.sql
+++ sql.sql
@@ -1,1 +1,24 @@
-SELECT * FROM lab2.users;
+create table audit_log
+(
+    id                int auto_increment
+        primary key,
+    event_datetime    datetime    not null,
+    username          varchar(45) not null,
+    event_description varchar(45) not null
+);
+
+create table users
+(
+    id                 int auto_increment
+        primary key,
+    username           varchar(45)       not null,
+    password           varchar(45)       not null,
+    login_attempts     int     default 0 null,
+    locked             tinyint default 0 null,
+    rools              tinyint default 0 null,
+    unlock_time        timestamp         null,
+    last_login_attempt timestamp         null
+);
+
+create index username_idx
+    on users (username);
